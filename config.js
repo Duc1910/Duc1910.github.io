cdb70ecd3b1ab09e47178718b67a40e64d29e55f
Duc1910 <92424790+Duc1910@users.noreply.github.com>
2021-11-24 15:11:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello ! 🖐🖐🖐',
+    introDesc: `Tớ có điều này muốn hỏi bạn, bạn nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Làm người yêu mình nhaaaaaaaa',
+    desc: 'bạn không trả lời hoặc thoát ra, tức là bạn đồng ý nha :> ',
+    btnYes: 'Có, tớ đồng ý',
+    btnNo: 'đéo bạn êiiiii ',
+    question:'Nói cho tớ biết tại sao bạn đồng ý đi -))',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'gửi',
+    reply: 'Tại vì thích :>>',
+    mess: 'ilove you 3000',
+    messDesc: '💬💬 Nhắn tin cho mình nha. 💬💬',
+    btnAccept: 'Ok lun ',
+    messLink: 'https://www.messenger.com/t/100008754768438'
+}
